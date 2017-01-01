c26789921d693216358ac0d0f9cf2b848f90b3cc
vogtn <vogtn@uw.edu>
2017-01-01 13:35:24 -0800

    freecodecamp description + pictures

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -18,19 +18,24 @@ function Experience({ children }) {
         </Link>
       </div>
       <div className="container">
-        <a href="https://github.com/vogtn/web">
+        <a href="https://www.freecodecamp.com/">
           <div id="boxOne" className="boxExperience fade-in one">
-            <img src="./img/reactwebsite.png"></img>
+            <img src="./img/freecodecamp.png"></img>
           </div>
         </a>
       </div>
       <br />
       <div className="box-description">FreeCodeCamp</div>
-      <p>CodeCampDescription here</p>
+      <p>This was my first foray into serious web development. Previously, I had taken classes in C# and Java which was a wonderful introduction to basic topics
+      and understanding how to think praigmatically. We mostly utilized low-level logic and objects to accomplish small projects, and even participated in Microsofts
+      "Hunt the Wumpus" competition. FreeCodeCamp gave me the tools and platform to understand solutions to web development. The lessons start by teaching basic arimitic with javascript
+      but then quickly transitions to using objects, jquery, and utlization of API's. This helped me understand the different tools and knowledge necessary for developing web appliations
+      in the current paradigm. It also gave me a sense of community and learning to utilize resources to learn on my own. From here I was able to branch out and find different stacks, and how
+      people learned and utilized them.</p>
       <div className="container">
-        <a href="https://github.com/vogtn/web">
+        <a href="query.me">
           <div id="boxOne" className="boxExperience fade-in two">
-            <img src="./img/reactwebsite.png"></img>
+            <img src="./img/query.png"></img>
           </div>
         </a>
       </div>
